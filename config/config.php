3baed5bc0f6a55fe0e67b806a26ba4eee392257c
roqueCM <rcarrillomar@uoc.edu>
2025-11-18 00:18:51 +0100

    modify config.php

--- config/config.php
+++ config/config.php
@@ -4,9 +4,9 @@
 
 if (!defined('DB_HOST')) define('DB_HOST', '127.0.0.1');
 if (!defined('DB_PORT')) define('DB_PORT', 3306);
-if (!defined('DB_NAME')) define('DB_NAME', 'uoc_transfers-1-1');
-if (!defined('DB_USER')) define('DB_USER', 'root');
-if (!defined('DB_PASS')) define('DB_PASS', '');
+if (!defined('DB_NAME')) define('DB_NAME', 'uoc_transfers');
+if (!defined('DB_USER')) define('DB_USER', 'roqueCM');
+if (!defined('DB_PASS')) define('DB_PASS', '8591RCM');
 if (!defined('DB_CHARSET')) define('DB_CHARSET', 'utf8mb4');
 
 // Define la ruta base para asegurar imports correctos
