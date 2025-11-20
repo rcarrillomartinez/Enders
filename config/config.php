a56174a4b9954571bb04bece3998ba84fc4f4544
elopez-uoc <elopezverge@uoc.edu>
2025-11-20 16:57:22 +0100

    Para que funcione con AWS
    Updated database configuration values for local environment.

--- config/config.php
+++ config/config.php
@@ -4,9 +4,9 @@
 
 if (!defined('DB_HOST')) define('DB_HOST', 'localhost');
 if (!defined('DB_PORT')) define('DB_PORT', 3306);
-if (!defined('DB_NAME')) define('DB_NAME', 'enders_db');
-if (!defined('DB_USER')) define('DB_USER', 'enders_user');
-if (!defined('DB_PASS')) define('DB_PASS', 'secret');
+if (!defined('DB_NAME')) define('DB_NAME', 'wordpress6');
+if (!defined('DB_USER')) define('DB_USER', 'wordpress6');
+if (!defined('DB_PASS')) define('DB_PASS', 'ZApzis0YqfKOY2Pw');
 if (!defined('DB_CHARSET')) define('DB_CHARSET', 'utf8mb4');
 
 // Define la ruta base para asegurar imports correctos
